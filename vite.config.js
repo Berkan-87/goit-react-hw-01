2e0f49f0d6a26365fab4df3b731222900c108e9b
Berkan-87 <sby.notess@gmail.com>
2025-04-21 18:57:40 +0300

    düzeltildi

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/goit-react-hw-01/', // Vercel'deki alt dizin
   plugins: [react()],
-  base: process.env.VERCEL ? '/' : '/goit-react-hw-01/',
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
