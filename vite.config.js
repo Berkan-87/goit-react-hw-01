00c666cc6eeaf1e23d4dea1b7a981aff173a28d2
Berkan-87 <sby.notess@gmail.com>
2025-04-22 22:53:22 +0300

    Remove unused Prisma setup

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
-
-
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/goit-react-hw-01/', // Vercel'deki alt dizin
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-    },
-  },
-});
-
+  base: '/goit-react-hw-01/',  // Github Pages'e özel base path
+})
